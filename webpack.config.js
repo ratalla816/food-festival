117a88bad7a2e2522258c119905f6753fc7e980d
ratalla816 <85460602+ratalla816@users.noreply.github.com>
2021-10-27 12:53:07 -0500

    updated entry points

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,18 @@ const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPl
 const path = require('path');
 const webpack = require("webpack");
 module.exports = {
-  entry: './assets/js/script.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.bundle.js'
+  entry: {
+    app: "./assets/js/script.js",
+    events: "./assets/js/events.js",
+    schedule: "./assets/js/schedule.js",
+    tickets: "./assets/js/tickets.js"
   },
+
+  output: {
+    filename: "[name].bundle.js",
+    path: __dirname + "/dist",
+  },
+
   plugins: [
     new webpack.ProvidePlugin({
       $: "jquery",
